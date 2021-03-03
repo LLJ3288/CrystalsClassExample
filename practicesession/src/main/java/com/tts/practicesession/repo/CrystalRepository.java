a445f33a51e7bb34b0eba6f213270f07f3311c0c
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-03-03 14:58:07 -0600

    Add files via upload

---
+++ practicesession/src/main/java/com/tts/practicesession/repo/CrystalRepository.java
@@ -0,0 +1,10 @@
+package com.tts.practicesession.repo;
+
+
+import com.tts.practicesession.model.Crystal;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CrystalRepository extends CrudRepository<Crystal, Long> {
+}
